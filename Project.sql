2cdb2f578feb1737d0728bd16e0afafffd15dda7
nikhiljjyes <58056946+nikhiljjyes@users.noreply.github.com>
2020-07-27 20:08:21 -0400

    Update Project.sql

--- Project.sql
+++ Project.sql
@@ -3,48 +3,48 @@ go
 USE [Wholesale Database Management System]
 go
 CREATE TABLE [Address] (
-  [Address ID] INT PRIMARY KEY,
-  [StreetName] VARCHAR(200),
-  [City] VARCHAR(30),
-  [State] VARCHAR(30),
-  [Country] VARCHAR(30),
-  [ZipCode] INT,
+  [Address ID] INT PRIMARY KEY NOT NULL ,
+  [StreetName] VARCHAR(200) NOT NULL ,
+  [City] VARCHAR(30) NOT NULL ,
+  [State] VARCHAR(30) NOT NULL ,
+  [Country] VARCHAR(30) NOT NULL ,
+  [ZipCode] INT NOT NULL ,
   );
   GO
 CREATE TABLE [Refund] (
-  [RefundOrderID] INT PRIMARY KEY,
+  [RefundOrderID] INT PRIMARY KEY NOT NULL ,
   [RefundStatus] VARCHAR(50),
   [RefundAmount] Decimal(20,15)
   );
   GO
 CREATE TABLE [Transaction] (
-  [TransactionID] INT PRIMARY KEY,
-  [TransactionMode] VARCHAR(20),
-  [TransactionDate] DATE,
-  [TransactionTime] TIME,
-  [TransactionAmount] DECIMAL(20,5),
-  [TransactionStatus] VARCHAR(20),
+  [TransactionID] INT PRIMARY KEY NOT NULL ,
+  [TransactionMode] VARCHAR(20) NOT NULL ,
+  [TransactionDate] DATE NOT NULL ,
+  [TransactionTime] TIME NOT NULL ,
+  [TransactionAmount] DECIMAL(20,5) NOT NULL ,
+  [TransactionStatus] VARCHAR(20) NOT NULL ,
 );
 GO
 CREATE TABLE [Category] (
-  [CategoryID] INT PRIMARY KEY,
-  [CategoryName] VARCHAR(50),
+  [CategoryID] INT PRIMARY KEY NOT NULL ,
+  [CategoryName] VARCHAR(50) NOT NULL ,
   );
   GO
 CREATE TABLE [Coupon] (
-  [CouponCode] Decimal(20,15) PRIMARY KEY,
-  [DiscountPercentage] DECIMAL(20,5),
-  [DateValidTill] DATE,
+  [CouponCode] Decimal(20,15) PRIMARY KEY NOT NULL ,
+  [DiscountPercentage] DECIMAL(20,5) NOT NULL ,
+  [DateValidTill] DATE NOT NULL ,
  );
  GO
 
 CREATE TABLE [Invoice] (
-  [InvoiceID] INT PRIMARY KEY,
-  [InvoiceDate] DATE,
-  [TotalAmount] Decimal(20,15),
-  [SalesTax] Decimal(20,15),
-  [Discount] Decimal(20,15),
-  [CouponCode] Decimal(20,15)
+  [InvoiceID] INT PRIMARY KEY NOT NULL ,
+  [InvoiceDate] DATE NOT NULL ,
+  [TotalAmount] Decimal(20,15) NOT NULL ,
+  [SalesTax] Decimal(20,15) NOT NULL ,
+  [Discount] Decimal(20,15) NOT NULL ,
+  [CouponCode] Decimal(20,15) NOT NULL 
   FOREIGN KEY ([CouponCode])
         REFERENCES [Coupon]([CouponCode])
         ON update CASCADE);
@@ -52,15 +52,13 @@ CREATE TABLE [Invoice] (
 CREATE INDEX [Coupon_FK] ON  [Invoice] ([CouponCode]);
 GO
 
-
-
 CREATE TABLE [Employee] (
-  [EmployeeID] INT PRIMARY KEY,
-  [FirstName] VARCHAR(20),
-  [LastName] VARCHAR(20),
-  [Address ID] INT ,
-  [TelephoneNumber] INT,
-  [Email] VARCHAR(50),
+  [EmployeeID] INT PRIMARY KEY NOT NULL ,
+  [FirstName] VARCHAR(20) NOT NULL ,
+  [LastName] VARCHAR(20) NOT NULL ,
+  [Address ID] INT NOT NULL ,
+  [TelephoneNumber] INT NOT NULL ,
+  [Email] VARCHAR(50) NOT NULL ,
   FOREIGN KEY ([Address ID])
         REFERENCES [Address]([Address ID])
         ON update CASCADE ON Delete CASCADE);
@@ -68,11 +66,11 @@ CREATE TABLE [Employee] (
 CREATE INDEX [Address_EMP_FK] ON  [Employee] ([Address ID]);
 
 CREATE TABLE [Order] (
-  [OrderID] INT PRIMARY KEY,
-  [OrderDate] DATE,
-  [OrderTime] TIME,
-  [OrderStatus] VARCHAR(20),
-  [EmployeeID] INT,
+  [OrderID] INT PRIMARY KEY NOT NULL ,
+  [OrderDate] DATE NOT NULL ,
+  [OrderTime] TIME NOT NULL ,
+  [OrderStatus] VARCHAR(20) NOT NULL ,
+  [EmployeeID] INT NOT NULL ,
   FOREIGN KEY ([EmployeeID])
         REFERENCES [Employee]([EmployeeID])
         ON update CASCADE ON Delete CASCADE
@@ -81,12 +79,12 @@ CREATE TABLE [Order] (
 CREATE INDEX [FK_O_Emp] ON  [Order] ([EmployeeID]);
 
 CREATE TABLE [Customer] (
-  [CustomerID] INT PRIMARY KEY,
-  [FirstName] VARCHAR(50),
-  [LastName] VARCHAR(50),
-  [TelephoneNumber] INT,
-  [Address ID] INT,
-  [EmailID] VARCHAR(100),
+  [CustomerID] INT PRIMARY KEY NOT NULL ,
+  [FirstName] VARCHAR(50) NOT NULL ,
+  [LastName] VARCHAR(50) NOT NULL ,
+  [TelephoneNumber] INT NOT NULL ,
+  [Address ID] INT NOT NULL ,
+  [EmailID] VARCHAR(100) NOT NULL ,
   FOREIGN KEY ([Address ID])
         REFERENCES [Address]([Address ID])
         ON update CASCADE ON Delete CASCADE
@@ -95,20 +93,20 @@ CREATE TABLE [Customer] (
 CREATE INDEX [FK_Cust_Add] ON  [Customer] ([Address ID]);
 
 CREATE TABLE [Returns] (
-  [ReturnRequestID] INT PRIMARY KEY,
-  [ReturnRequestDate] DATE,
-  [ReturnRequestTime] TIME,
-  [RefundOrderID] INT,
+  [ReturnRequestID] INT PRIMARY KEY NOT NULL ,
+  [ReturnRequestDate] DATE NOT NULL ,
+  [ReturnRequestTime] TIME NOT NULL ,
+  [RefundOrderID] INT NOT NULL ,
   FOREIGN KEY ([RefundOrderID])
         REFERENCES [Refund]([RefundOrderID])
         ON update CASCADE ON Delete CASCADE);
 
 CREATE INDEX [FK_RETURN] ON  [Returns] ([RefundOrderID]);
 CREATE TABLE [Item] (
-  [ItemNo] INT PRIMARY KEY,
-  [ItemName] VARCHAR(50),
-  [CustomerReviews] VARCHAR(200),
-  [CategoryID] INT,
+  [ItemNo] INT PRIMARY KEY NOT NULL ,
+  [ItemName] VARCHAR(50) NOT NULL ,
+  [CustomerReviews] VARCHAR(200) NOT NULL ,
+  [CategoryID] INT NOT NULL ,
   FOREIGN KEY ([CategoryID])
         REFERENCES [Category]([CategoryID])
         ON update CASCADE ON Delete CASCADE
@@ -117,11 +115,11 @@ CREATE TABLE [Item] (
 CREATE INDEX [FK_Item_cat] ON  [Item] ([CategoryID]);
 
 CREATE TABLE [Distributor] (
-  [DistributorID] INT PRIMARY KEY,
-  [DistributorName] VARCHAR(50),
-  [TelephoneNumber] INT,
-  [Address ID] INT,
-  [EmailID] VARCHAR(100),
+  [DistributorID] INT PRIMARY KEY NOT NULL ,
+  [DistributorName] VARCHAR(50) NOT NULL ,
+  [TelephoneNumber] INT NOT NULL ,
+  [Address ID] INT NOT NULL ,
+  [EmailID] VARCHAR(100) NOT NULL ,
   FOREIGN KEY ([Address ID])
         REFERENCES [Address]([Address ID])
         ON update CASCADE ON Delete CASCADE
@@ -130,10 +128,10 @@ CREATE TABLE [Distributor] (
 CREATE INDEX [FK_D_Add] ON  [Distributor] ([Address ID]);
 
 CREATE TABLE [Transportation Vendor] (
-  [VendorID] INT PRIMARY KEY,
-  [VendorName] VARCHAR(30),
-  [Address ID] INT,
-  [EmailID] VARCHAR(150),
+  [VendorID] INT PRIMARY KEY NOT NULL ,
+  [VendorName] VARCHAR(30) NOT NULL ,
+  [Address ID] INT NOT NULL ,
+  [EmailID] VARCHAR(150) NOT NULL ,
   FOREIGN KEY ([Address ID])
         REFERENCES [Address]([Address ID])
         ON update CASCADE ON Delete CASCADE  
@@ -142,12 +140,12 @@ CREATE TABLE [Transportation Vendor] (
 CREATE INDEX [FK_T_Address] ON  [Transportation Vendor] ([Address ID]);
 
 CREATE TABLE [Shipping] (
-  [ShippingLabelNo] VARCHAR(50) PRIMARY KEY,
-  [Origin] VARCHAR(50),
-  [Destination] VARCHAR(30),
-  [ShippingStatus] VARCHAR(18),
-  [ShippingCost] DECIMAL(20,15),
-  [VendorID] INT,
+  [ShippingLabelNo] VARCHAR(50) PRIMARY KEY NOT NULL,
+  [Origin] VARCHAR(50) NOT NULL,
+  [Destination] VARCHAR(30) NOT NULL,
+  [ShippingStatus] VARCHAR(18) NOT NULL,
+  [ShippingCost] DECIMAL(20,15) NOT NULL,
+  [VendorID] INT NOT NULL,
   FOREIGN KEY ([VendorID])
         REFERENCES [Transportation Vendor]([VendorID])
         ON update CASCADE ON Delete CASCADE
@@ -156,10 +154,10 @@ CREATE TABLE [Shipping] (
 CREATE INDEX [FK_Vendor] ON  [Shipping] ([VendorID]);
 
 CREATE TABLE [CustomerReturnsOrder] (
-  [CustomerID] INT,
-  [OrderID] INT,
-  [InvoiceID] INT,
-  [ReturnRequestID] INT,
+  [CustomerID] INT NOT NULL,
+  [OrderID] INT NOT NULL,
+  [InvoiceID] INT NOT NULL,
+  [ReturnRequestID] INT NOT NULL,
   CONSTRAINT PK_CRO PRIMARY KEY([CustomerID],[OrderID],[InvoiceID],[ReturnRequestID])
   );
   GO
@@ -186,22 +184,20 @@ CREATE INDEX [PK_CRO, FK_CRO_4] ON  [CustomerReturnsOrder] ([ReturnRequestID]);
 GO
 
 CREATE TABLE [Price] (
-  [ItemNo] INT,
-  [RetailPrice] DECIMAL(20,5),
-  [WholesalePrice] DECIMAL(20,5),
-  [Discount] DECIMAL(20,5),
+  [ItemNo] INT NOT NULL,
+  [RetailPrice] DECIMAL(20,5) NOT NULL,
+  [WholesalePrice] DECIMAL(20,5) NOT NULL,
+  [Discount] DECIMAL(20,5) NOT NULL,
   FOREIGN KEY ([ItemNo])
         REFERENCES [Item]([ItemNo])
         ON update CASCADE ON Delete CASCADE);
 
 CREATE INDEX [FK_ITEM] ON  [Price] ([ItemNo]);
 
-
-
 CREATE TABLE [Inventory] (
-  [ItemNo] INT,
-  [QuantityInStock] INT,
-  [Inventory Costs] DECIMAL(20,15),
+  [ItemNo] INT NOT NULL,
+  [QuantityInStock] INT NOT NULL,
+  [Inventory Costs] DECIMAL(20,15) NOT NULL,
    FOREIGN KEY ([ItemNo])
         REFERENCES [Item]([ItemNo])
         ON update CASCADE ON Delete CASCADE
@@ -209,14 +205,12 @@ CREATE TABLE [Inventory] (
 
 CREATE INDEX [FK_I_ITEM] ON  [Inventory] ([ItemNo]);
 
-
-
 CREATE TABLE [CustomerOrder] (
-  [CustomerID] INT,
-  [OrderID] INT,
-  [InvoiceID] INT,
-  [TransactionID] INT,
-  [ShippingLabelNo] VARCHAR(50),
+  [CustomerID] INT NOT NULL,
+  [OrderID] INT NOT NULL,
+  [InvoiceID] INT NOT NULL,
+  [TransactionID] INT NOT NULL,
+  [ShippingLabelNo] VARCHAR(50) NOT NULL,
   CONSTRAINT PK_CO PRIMARY KEY([CustomerID],[OrderID],[InvoiceID],[TransactionID],[ShippingLabelNo])
 );
  GO
@@ -243,13 +237,9 @@ CREATE INDEX [PK_CO, FK_CO_4] ON  [CustomerOrder] ([TransactionID]);
 
 CREATE INDEX [PK_CO, FK_CO_5] ON  [CustomerOrder] ([ShippingLabelNo]);
 
-
-
-
-
 CREATE TABLE [ItemDistributor] (
-  [ItemNo] INT,
-  [DistributorID] INT,
+  [ItemNo] INT NOT NULL,
+  [DistributorID] INT NOT NULL,
     CONSTRAINT PK_ID PRIMARY KEY([ItemNo],[DistributorID])
 );
 ALTER TABLE [ItemDistributor]
@@ -263,9 +253,9 @@ CREATE INDEX [PK_ID, FK_ID_2] ON  [ItemDistributor] ([DistributorID]);
 
 
 CREATE TABLE [OrderItem] (
-  [OrderID] INT,
-  [ItemNo] INT,
-  [Quantity] INT,
+  [OrderID] INT NOT NULL,
+  [ItemNo] INT NOT NULL,
+  [Quantity] INT NOT NULL,
   CONSTRAINT PK_OI PRIMARY KEY([OrderID],[ItemNo])
 );
   ALTER TABLE [OrderItem]
@@ -278,5 +268,5 @@ CREATE INDEX [PK_OI, FK_OI_1] ON  [OrderItem] ([OrderID]);
 
 CREATE INDEX [PK_OI, FK_OI_2] ON  [OrderItem] ([ItemNo]);
 
-
+--INSERT DATA 
 
